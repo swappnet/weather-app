3ff78a4d0df9b99d8cc1768cd6363a32bccaa28f
Swappnet <advance11ua@gmail.com>
2023-07-18 10:31:50 +0300

    added header for favorites page

--- src/views/FavoritesView.vue
+++ src/views/FavoritesView.vue
@@ -4,11 +4,30 @@
 
 <template>
     <header>
-        Favorites
+        <h1>Saved locations</h1>
+        <p>Maximum 5 location</p>
     </header>
     <main>
 
     </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+h1 {
+    font-family: Inter, sans-serif;
+    color: #434343;
+    font-size: 1.5rem;
+    font-weight: 600;
+    line-height: normal;
+    letter-spacing: -0.95px;
+}
+
+p {
+    font-family: Roboto, sans-serif;
+    color: #797979;
+    font-size: .9rem;
+    font-weight: 400;
+    line-height: normal;
+    letter-spacing: 0.06px;
+}
+</style>
